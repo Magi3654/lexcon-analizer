b26075fe8d80f71f9b0b671ab8c4d60dc8dd88c1
Magi3654 <magi3654@gmail.com>
2023-10-25 11:04:00 -0700

    fixing resposivenest at textarea

--- src/app/ components/Header.jsx
+++ src/app/ components/Header.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='p-8 bg-violet-950 space-y-5'>
+    <header className='p-8 bg-violet-950 space-y-5 w-screen'>
         <div className='font-mono'>
             <h1 className='text-5xl text-violet-200 font-bold'>Lexcon-Analizer </h1>
-        </div>
-        
+        </div> 
 
         <nav>
             <ul className='flex justify-evenly uppercase font-mono'>
--- src/app/ejercicio1/page.jsx
+++ src/app/ejercicio1/page.jsx
@@ -21,7 +21,7 @@ function Ejercicio01(){
   }
   
   return(
-    <div className='text-center h-screen bg-lime-200 font-mono'>
+    <div className='text-center h-screen w-screen bg-lime-200 font-mono'>
         <div className='text-information p-4'>
           <h3 className=' font-bold text-4xl text-violet-950'>Inserta tu archivo</h3>
           <input className='file-upload-input font-bold text-2xl text-violet-950 m-2 p-4' type='file' accept='.txt' multiple onChange={e=>{changeText(e)}}/>
@@ -31,7 +31,8 @@ function Ejercicio01(){
         </div>
         
         <div className='text-center'>
-          <textarea  className='m-4 text-slate-950 font-mono text-2xl bg-violet-200 rounded-md' value={ShowText || ''} onChange={changeTextArea} rows="15" cols="90" >
+          <textarea  className='m-4 text-slate-950 font-mono text-2xl bg-violet-200 rounded-md w-full' value={ShowText || ''} 
+            onChange={changeTextArea} rows="15" >
             {ShowText}
           </textarea> 
         </div>
